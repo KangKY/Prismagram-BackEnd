5fff217a083ddd65ee4cfc4450733aa0a67384fe
Kang_KI <ruddlf4933@gmail.com>
2020-04-10 18:19:18 +0900

    Comment of Post 수정

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -3,7 +3,7 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Post: {
     files: ({ id }) => prisma.post({ id }).files(),
-    comments: ({ id }) => prisma.post({ id }).comments(),
+    comments: ({ id }) => prisma.post({ id }).comments({where: { parent:null } }),
     recentcomments :({ id }) => prisma.post({ id }).comments({last:2}),
     user: ({ id }) => prisma.post({ id }).user(),
     likes: ({ id }) => prisma.post({ id }).likes(),
