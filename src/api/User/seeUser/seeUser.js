6515199d7df521f4669b1176228ad3b4a7dbdcf0
Kang_KI <ruddlf4933@gmail.com>
2020-04-10 16:38:49 +0900

    seeUser API 수정

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -3,8 +3,15 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     seeUser: async (_, args) => {
-      const { username } = args;
-      return prisma.user({ username });
+      const { username, id } = args;
+      if(id) {
+        return await prisma.user({ id });
+      } else if(username) {
+        return prisma.user({ username });
+      } else {
+        throw Error("need info");
+      }
+      
       //return await prisma.user({ id });
     }
   }
