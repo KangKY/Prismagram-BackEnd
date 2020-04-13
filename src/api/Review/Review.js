d22bd79cb50ecefba99cea06972b90a6696fbfb1
Kang_KI <ruddlf4933@gmail.com>
2020-04-13 18:07:05 +0900

    Review.js 서브쿼리 수정

--- src/api/Review/Review.js
+++ src/api/Review/Review.js
@@ -2,7 +2,7 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Review: {
-    post:({ id }) => prisma.like({ id }).post(),
-    user:({ id }) => prisma.like({ id }).user()
+    post:({ id }) => prisma.review({ id }).post(),
+    user:({ id }) => prisma.review({ id }).user()
   }
 }
